7011b5d094d2d36c74619d6aae498d9b9acc9805
hishamkhattab1 <89085957+hishamkhattab1@users.noreply.github.com>
2024-04-18 12:21:47 +0200

    Update CommonMethods.java

--- src/main/java/seldocker/CommonMethods.java
+++ src/main/java/seldocker/CommonMethods.java
@@ -12,23 +12,22 @@ public class CommonMethods {
 	public static void runTerminalCommand(String command,String logText)
 	{
 		try {
-			String path = System.getProperty("user.dir");
-			ProcessBuilder builder = new ProcessBuilder(
-					"cmd.exe", "/c", "cd \""+path+"\" && "+command);
-			builder.redirectErrorStream(true);
-			Process p = builder.start();
-			BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			String line;
-			while (true) {
-				line = r.readLine();
-				if (line.contains(logText)) { 
-					Thread.sleep(5000);
-					System.out.println(line);
-					break; 
-				}
+            String path = System.getProperty("user.dir");
+            ProcessBuilder builder = new ProcessBuilder(
+                    "sh", "-c", "cd \"" + path + "\" && " + command);
+            builder.redirectErrorStream(true);
+            Process p = builder.start();
+            BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line;
+            while ((line = r.readLine()) != null) {
+                if (line.contains(logText)) {
+                    Thread.sleep(5000);
+                    System.out.println(line);
+                    break;
+                }
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
+		} catch (IOException | InterruptedException e) {
+            e.printStackTrace();
 		}
 
 	}
